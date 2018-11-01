20f848cab8f6ca6f8ef213cc6d010995373f4e21
jochigt87 <contrerasjochimin@gmail.com>
2018-11-01 12:22:11 -0400

    page
    Python Exercises

---
+++ exercise_102.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Function Exercises
+
+# Exercise 102:Reduce Measures.
+
+"""
+Many recipe books still use cups, tablespoons and teaspoons to describe the
+volumes of ingredients used when cooking or baking. While such recipes are
+easy enough to follow if you have the appropiate measuring cups and spoons,
+they can be difficult to double, triple or quadruple when cooking Christmas
+dinner for the entire extended family. For example, a recipe that calls for 4 tablespoons of an ingredient requires 16 tablespoons when quadrupled. However, 16 tablespoons would be better expressed (and easier to measure) as 1 cup.
+
+    Write a function that express that imperial volume using the largest
+units posible. The function will take the number of units as its first parameter, and the unit of measure(cup, tablespoon or teaspoon) as its second parameter. Return a string representing the measure using the largest possible units as the function's only result.
+
+For example, if the function is provided with parameters representing 59 teaspoons then it should return the string "1 cup, 3 tablespoon, 2 teaspoon".
+
+Hint: One cup is equivalent to 16 tablespoons. One tablespoon is equivalent to 3 teaspoons.
+"""
