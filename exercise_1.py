14cd34895ecda10754cd16246552a24b23c8c8d4
jochigt87 <contrerasjochimin@gmail.com>
2018-11-01 23:54:04 -0400

    Python WorkBook

--- exercise_1.py
+++ exercise_1.py
@@ -3,8 +3,10 @@
 
 #
 
-# Exercise :.
+# Exercise 1: Mailing Address.
 
 """
-
+Create a program that display your name and complete mailing address formatted
+in the manner that you would usually see it on the outside of an envelope.
+Your program does not need  to read any input from the user.
 """
