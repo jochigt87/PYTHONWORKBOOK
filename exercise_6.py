77748d98ebaf2d08e410a18d169979041ffbe744
jochigt87 <contrerasjochimin@gmail.com>
2018-11-03 22:01:45 -0400

    Python Work Book

--- exercise_6.py
+++ exercise_6.py
@@ -3,8 +3,15 @@
 
 # Introduction to Programming Exercises.
 
-# Exercise :.
+# Exercise 6: Tax and Tip.
 
 """
-
+The program that you create for this exercise will begin by reading the cost
+of a meal ordered at a restaurant from the user. Then you program will compute
+the tax and tip for the meal. Use your local tax rate when computing the
+amount of tax owing. Compute the tip as 18 percent of the meal amount
+(without the tax). The output from your program should include the tax amount,
+the tip amount, and the grand total for the meal inclufing both the tip.
+Format the output so that all of the values are displayed using two decimal
+places.
 """
--- exercise_7.py
+++ exercise_7.py
@@ -3,8 +3,12 @@
 
 # Introduction to Programming Exercises.
 
-# Exercise :.
+# Exercise 7: Sum of the First n Positive Integers.
 
 """
+Write a program that reads a positive integer, n, from the user and then
+displayed the sum of all the integers from 1 to n. The sum of the first n
+positive integers can be computed using the formula:
 
+                sum = (n)(n + 1)/2
 """
