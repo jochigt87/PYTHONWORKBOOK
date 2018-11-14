81ee30e3d47137dccc5ca5fda077ac69bfaef282
jochigt87 <contrerasjochimin@gmail.com>
2018-11-14 09:45:38 -0400

    Python Work Book

--- exercise_122.py
+++ exercise_122.py
@@ -3,8 +3,37 @@
 
 # Lists Exercises.
 
-# Exercise :.
+# Exercise 122:Tokenizing a String.
 
 """
+Tokenizing is the proccess of converting a string into a list  of substrings,
+known as tokens. In many circumstances, a list of tokens is far easier to work
+with than the original string because the original string may have irregular
+spacing. In some cases substantial work is also required to determine where
+one token ands and the next one begins.
 
+In a mathematical expression, tokens are items such as operators, numbers and
+parentheses. Some tokens, sucj as *, /, ^, (and) are easy to identify because
+the token is a single character, and the character is never part of another
+token. The + and - symbols are little bit more challenging to handle because
+they might represent the addition or subtraction operator, or they might be
+part of a number token.
+
+Hint: A + or - is an operator if the non-whitespace character immediately
+before it is part of a number, or if the non-whitespace character immediately
+before it is a close parenthesis. Otherwise it is part of a number.
+
+    Write a function that takes a string containing a a mathematical
+expression as its only parameter and breaks it into a list of tokens. Each
+token should be a parenthesis, an operator, or a number with an optional
+leading + or - (for simplicity we will only work with integers in this
+problem). Return the list of tokens as the function's result.
+
+    You may assume that the string passed to your function always contains a
+valid mathematical expression consisting of parentheses, operators and
+integers. However, your function must handle variable amounts of whitespace
+between these elements. include a main program that demonstrates your
+tokenizing function by reading an expression from the user and printing the
+list of tokens. Ensure that the main program will not run when the file
+containing your solution is imported into another program.
 """
--- exercise_123.py
+++ exercise_123.py
@@ -3,8 +3,56 @@
 
 # Lists Exercises.
 
-# Exercise :.
+# Exercise 123: Infix to Postfix.
 
 """
+Mathematical expressions are often written in infix form, where operators
+appear between the operands on which they act. While this is a common form, it
+is also possible to express mathematical expressions in postfix form, where
+the operator appears after both operands. For example, the infix expression 3
++ 4 is written as 3 4 + in postfix form. One can convert an infix expression
+to postfix form using the following algorithm:
 
+    Create a new empty list, operators
+    Create a new empty list, postfix
+
+    For each token in the infix expression
+        If the token is an integer then
+            Add the token to the end of postfix
+        If the token is an operator then
+            While operators is not empty and
+                the last item in operators is not an open parenthesis and
+                precedence(token) < precedence(last item in operators) do
+             Remove the last item from operators and add it to postfix
+            Add toke to the end of operators
+        If the token is an open parenthesis then
+            Add token to the end of operators
+        If the token is a close parenthesis then
+            While the last item in operators is not an open parenthesis do
+                Remove the last item from operators and add it to postfix
+            Remove the open parenthesis from operators
+
+    While operators is not the empty list do
+        Remove the last item from operators and add it to postfix
+
+    Return postfix as the result of the algorithm
+
+    Use your solution to Exercise 122 to tokenize a mathematical expression.
+Then use the algorithm above to transform the expression from infix form to
+postfix form. Your code that implements the precceding algorithm should reside
+in a function that takes a list of tokens representing an infix expression as
+its only parameter. It should return a list of tokens representing the
+equivalent postfix expression as its only result. Include a main program that
+demonstrates your infix to postfix function by reading an expression from the
+user in infix form and displaying it in postfix form.
+
+The porpuse of converting from infix form to postfix form will become apparent
+when you read Exercise 124. You may find your solutions to Exercises 90 and 91
+helpful when completiong this problem.
+
+The algorithm provided in Exercises 123 and 124 do not perform any error
+checking. As a result, you may crash your program or receive incorrect results
+if you provide them with invalid input. These algorithm can be extended to
+detect invalid input and respond to it in a reasonable manner. Doing so is
+left as an indepent study exercise for the interested student.
 """
