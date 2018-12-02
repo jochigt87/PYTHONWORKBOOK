168071fc47a165357c9ecfc8a610c3c6cc91bce5
jochigt87 <contrerasjochimin@gmail.com>
2018-12-02 10:06:27 -0400

    Exercises Python WorkBook

--- exercise_29.py
+++ exercise_29.py
@@ -11,3 +11,4 @@ Celsius. Then your program should display the equivalent temperature in
 degrees Fahrenheit and degrees Kelvin. The calculations needed to convert
 between different units of temperature can be found on the internet.
 """
+
